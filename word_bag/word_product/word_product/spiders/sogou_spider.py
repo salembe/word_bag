994b50029b3841c49136973e641ea32031419941
yuyou <88315203@qq.com>
2017-11-23 14:34:42 +0800

    爬虫页码

--- word_bag/word_product/word_product/spiders/sogou_spider.py
+++ word_bag/word_product/word_product/spiders/sogou_spider.py
@@ -16,24 +16,25 @@ import scrapy
 
 class SogouSpider(scrapy.Spider):
     name = 'sogou'
-    urls = ['https://pinyin.sogou.com/dict/cate/index/360',  # 城市信息
-            'https://pinyin.sogou.com/dict/cate/index/1',  # 自然科学
-            'https://pinyin.sogou.com/dict/cate/index/76',  # 社会科学
-            'https://pinyin.sogou.com/dict/cate/index/96',  # 工程应用
-            'https://pinyin.sogou.com/dict/cate/index/127',  # 农林渔畜
-            'https://pinyin.sogou.com/dict/cate/index/132',  # 医学医药
-            'https://pinyin.sogou.com/dict/cate/index/436',  # 电子游戏
-            'https://pinyin.sogou.com/dict/cate/index/154',  # 艺术设计
-            'https://pinyin.sogou.com/dict/cate/index/389',  # 生活百科
-            'https://pinyin.sogou.com/dict/cate/index/367',  # 运动休闲
-            'https://pinyin.sogou.com/dict/cate/index/31',  # 人文科学
-            'https://pinyin.sogou.com/dict/cate/index/403',  # 娱乐休闲
-            ]
-    total_page = 300
+    urls = {'https://pinyin.sogou.com/dict/cate/index/360': 10,  # 城市信息
+            'https://pinyin.sogou.com/dict/cate/index/1': 33,  # 自然科学
+            'https://pinyin.sogou.com/dict/cate/index/76': 53,  # 社会科学
+            'https://pinyin.sogou.com/dict/cate/index/96': 87,  # 工程应用
+            'https://pinyin.sogou.com/dict/cate/index/127': 11,  # 农林渔畜
+            'https://pinyin.sogou.com/dict/cate/index/132': 34,  # 医学医药
+            'https://pinyin.sogou.com/dict/cate/index/436': 120,  # 电子游戏
+            'https://pinyin.sogou.com/dict/cate/index/154': 21,  # 艺术设计
+            'https://pinyin.sogou.com/dict/cate/index/389': 93,  # 生活百科
+            'https://pinyin.sogou.com/dict/cate/index/367': 19,  # 运动休闲
+            'https://pinyin.sogou.com/dict/cate/index/31': 113,  # 人文科学
+            'https://pinyin.sogou.com/dict/cate/index/403': 133,  # 娱乐休闲
+            }
+
+    # total_page = 300
 
     def start_requests(self):
-        for url in self.urls:
-            for i in range(self.total_page):
+        for url, total_page in self.urls.iteritems():
+            for i in range(total_page):
                 yield scrapy.Request(url + '/default/' + str(i + 1),
                                      callback=self.parse_channel,
                                      dont_filter=True)
